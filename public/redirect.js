6823bf1ecc8e1c0a4476ab5b5c84b1b06c0a9bc3
SilasDemez <silasdemez@gmail.com>
2021-11-04 15:52:20 +0100

    cleanup

--- public/redirect.js
+++ public/redirect.js
@@ -5,35 +5,44 @@ document.cookie = "client_id=; Max-Age=-99999999;";
 document.cookie = "client_secret=; Max-Age=-99999999;";
 */
 
+//console.log(document.cookie);
+
 let client_id = null;
 let client_secret = null;
 const auth_link = "https://www.strava.com/oauth/token";
 
-console.log(document.cookie);
-
+// detecting if the user already connected strav ato the app
+// it is done by checking if there are any existing cookies
 if (document.cookie != ''){
+    console.log("You previously connected strava");
 
-    console.log("You previously logged in to strava");
-    let cookies = decodeURIComponent(document.cookie).split(';');
-    let cleaned_cookies = []
+    let cleaned_cookies = [];   // where the cookie values will be in second position of the array eg.:  refresh_token = cleaned_cookies[1][1];
+    let cookies = decodeURIComponent(document.cookie).split(';');   // get cookies and put them in an array
+
+    // split up the cookies array into 
+    // [0 (first cookie)][0] --> cookie name
+    // [0 (first cookie)][1] --> cookie value
     cookies.forEach(cookie => {
         cleaned_cookies.push(cookie.split('='));
     })
-    console.log(cleaned_cookies);
+
+    // getting the values from cookie array (at position 1 the value is located, at position 0 the name is located)
     refresh_token = cleaned_cookies[1][1];
     client_id = cleaned_cookies[2][1];
     client_secret = cleaned_cookies[3][1];
-    console.log("Refresh token found in cookies: " + refresh_token);
-    console.log("Client id found in cookies: " + client_id);
-    console.log("Client secert found in cookies: " + client_secret);
+
+    // get new acces token by making a post request with the refresh token
     reAuthorize();
 
 }else{
+    console.log("First login to strava");
+
+    // get API key like client_id and client_secret that are securely located on a .env file on the server
     getAPI_KEY().then((res) => {
-        console.log("Client secret: " + res.client_secret);
         client_id = res.client_id;
         client_secret = res.client_secret;
     
+        // begin authentification process
         auth();
     });
 }
@@ -41,59 +50,44 @@ if (document.cookie != ''){
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // functions
 
+// get API key like client_id and client_secret that are securely located on a .env file on the server
 async function getAPI_KEY() {
     const response = await fetch('/api');
-    const data = await response.json();
+    const keys = await response.json();
 
-    return data
+    return keys
 }
 
+
+//begin authentification process
 function auth(){
 
     // getting the authentication token from the address bar
     auth_token = location.search;
     auth_token = cleanupAuthToken(auth_token);
-    console.log("Authentication Token: " + auth_token)
 
     let access_token = null;
     let refresh_token = null;
 
+    // getting the access and refresh token by making a post request with the authentification code
     getTokens(auth_token).then(function (res) {
         access_token = res.access_token;
         refresh_token = res.refresh_token;
 
+        // get the activities of the authenticated athlete
         getActivities(res);
 
     });
 }
 
-
+// extract the AuthToken out of the search bar content
 function cleanupAuthToken(str) {
     return str.split("&")[1].slice(5);
 }
 
-function getCookie(cname) {
-    let name = cname + "=";
-    console.log(name);
-    let decodedCookie = decodeURIComponent(document.cookie);
-    console.log(decodedCookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      console.log(c);
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-
+// get the activities of the authenticated athlete
 async function getActivities(res) {
 
-    console.log("Access token: " + res.access_token);
     document.cookie = "access_token=" +  res.access_token + ";"
     document.cookie = "refresh_token=" +  res.refresh_token + ";"
     document.cookie = "client_id=" + client_id + ";"
@@ -101,11 +95,13 @@ async function getActivities(res) {
 
     const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
 
-    
+    // get the activities
     fetch(activities_link)
     .then((res) => res.json())
     .then((json) => {
         console.log(json);
+
+        // write them into the document
         str = ""
         for (let i = 0; i < json.length; i++) {
             str += json[i].name + "     " + json[i].distance/1000 + "km\n";
@@ -117,10 +113,7 @@ async function getActivities(res) {
 
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
+// getting the access and refresh token by making a post request with the authentification code
 async function getTokens(auth_token) {
     const response = await fetch(auth_link, {
         method: 'post',
@@ -132,7 +125,7 @@ async function getTokens(auth_token) {
 
         body: JSON.stringify({
 
-            client_id: '67034',
+            client_id: client_id,
             client_secret: client_secret,
             code: auth_token,
             grant_type: 'authorization_code'
@@ -142,10 +135,8 @@ async function getTokens(auth_token) {
     return response.json();
 }
 
-
+// get new acces token by making a post request with the refresh token
 function reAuthorize() {
-    console.log("Client secret: " + client_secret);
-    console.log(refresh_token);
 
     fetch(auth_link, {
         method: 'post',
@@ -164,27 +155,4 @@ function reAuthorize() {
         })
     }).then(res => res.json())
         .then(res => getActivities(res))
-}
-
-
-async function reAuthorize1() {
-    const response = await fetch(auth_link, {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-
-        },
-
-        body: JSON.stringify({
-
-            client_id: client_id,
-            client_secret: client_secret,
-            grant_type: 'refresh_token',
-            refresh_token: refresh_token
-        })
-    })
-
-
-    getActivities(response.json());
 }
