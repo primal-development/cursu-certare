111aec17858060805ce11d099defb33bc9645fc0
SilasDemez <silasdemez@gmail.com>
2021-12-29 21:34:47 +0100

    added webhooks to the server👌

--- app.js
+++ app.js
@@ -124,3 +124,34 @@ app.post("/key", (request,response) => {
 
 
 //testconnection();
+
+
+// webhooks
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+  console.log("webhook event received!", req.query, req.body);
+  res.status(200).send('EVENT_RECEIVED');
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+  // Your verify token. Should be a random string.
+  const VERIFY_TOKEN = "abcdefgh";
+  // Parses the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Verifies that the mode and token sent are valid
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.json({"hub.challenge":challenge});  
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
